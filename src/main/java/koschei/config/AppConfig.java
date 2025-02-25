dd31773bd78969b9966da17ffffd74633669dcc6
admin <mametkadyrov.y@gmail.com>
2025-02-25 20:52:22 +0300

    koschei

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -2,6 +2,7 @@ package koschei.config;
 
 import koschei.models.Island2;
 import koschei.models.Wood3;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
 
     @Bean
+    @Autowired
     public static Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
     }
